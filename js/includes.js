4e392891a63a8c531cd78b3234c7e55a7b6bc1e7
Ariadna-S708 <ariadnacursos518@gmail.com>
2025-06-01 20:36:47 -0300

    Correccion de rutas absolutas

--- js/includes.js
+++ js/includes.js
@@ -1,10 +1,10 @@
-fetch("includes/header.html")
+fetch("/includes/header.html")
     .then((res) => res.text())
     .then((html) => {
     document.getElementById("include-header").innerHTML = html;
     });
 
-fetch("includes/footer.html")
+fetch("/includes/footer.html")
     .then((res) => res.text())
     .then((html) => {
     document.getElementById("include-footer").innerHTML = html;
